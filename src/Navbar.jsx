8d39ec4d652bda9ac7fd5e4af290c94a102484dd
Viktor Gustafsson <viktor.gustafsson1@hotmail.com>
2026-08-28 08:05:40 +0200

    feat: add menu toggle state and NavMenu component to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,10 +5,15 @@ import {
   UserIcon,
   MagnifyingGlassIcon,
 } from "@phosphor-icons/react";
+import NavMenu from "./NavMenu";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="flex justify-center bg-bg w-full py-4 border-b">
+      {isMenuOpen && <NavMenu onClose={() => setIsMenuOpen(false)} />}
       <div className="flex justify-between items-center sm:grid sm:grid-cols-3 w-[90%]">
         {/* Logga */}
         <div className="flex items-center gap-1 order-1 sm:order-2 sm:justify-self-center">
@@ -24,33 +29,41 @@ export default function Navbar() {
         <div className="flex items-center gap-4 text-text order-2 sm:contents">
           <div className="flex items-center gap-4 sm:order-1 sm:justify-self-start">
             <div className="py-2 px-2 rounded-full cursor-pointer hover:bg-card">
-              <ListIcon
-                weight="thin"
-                className="w-[26px] h-[26px] xl:w-8 xl:h-8"
-              />
+              <button onClick={() => setIsMenuOpen((prev) => !prev)}>
+                <ListIcon
+                  weight="thin"
+                  className="w-[26px] h-[26px] xl:w-8 xl:h-8"
+                />
+              </button>
             </div>
             <div className="flex items-center px-2 py-2 rounded-full gap-2 cursor-pointer hover:bg-card ">
-              <MagnifyingGlassIcon
-                weight="thin"
-                className="w-[26px] h-[26px] xl:w-8 xl:h-8"
-              />
-              <p className="hidden sm:block text-text font-body">Search</p>
+              <button>
+                <MagnifyingGlassIcon
+                  weight="thin"
+                  className="w-[26px] h-[26px] xl:w-8 xl:h-8"
+                />
+                <p className="hidden sm:block text-text font-body">Search</p>
+              </button>
             </div>
           </div>
           {/* Kundvagn */}
           <div className="flex gap-2 sm:order-3 sm:justify-self-end">
             <div className="hidden cursor-pointer xl:flex xl:items-center xl:gap-1 py-2 px-2 rounded-full hover:bg-card">
-              <UserIcon
-                weight="thin"
-                className="w-[26px] h-[26px] xl:w-8 xl:h-8"
-              />
-              <p className="text-text font-body">Log In</p>
+              <button>
+                <UserIcon
+                  weight="thin"
+                  className="w-[26px] h-[26px] xl:w-8 xl:h-8"
+                />
+                <p className="text-text font-body">Log In</p>
+              </button>
             </div>
             <div className="py-2 px-2 rounded-full cursor-pointer hover:bg-card">
-              <ShoppingCartIcon
-                weight="thin"
-                className="w-[26px] h-[26px] xl:w-8 xl:h-8"
-              />
+              <button>
+                <ShoppingCartIcon
+                  weight="thin"
+                  className="w-[26px] h-[26px] xl:w-8 xl:h-8"
+                />
+              </button>
             </div>
           </div>
         </div>
