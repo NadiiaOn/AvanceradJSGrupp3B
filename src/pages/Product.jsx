ebf7c134af48ae37ca49639fcb10421e9c6433cb
NadiiaOn <nadiia.onopriienko@gmail.com>
2026-08-30 19:32:08 +0200

    implement the main page structure

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import fetchProducts from "../api/fetchProducts";
+//import CreateProductForm from "../components/CreateProductForm"; //ToDo: create product form component
 
-export default function ProductModal() {
+export default function ProductPage() {
     const [isOpen, setIsOpen] = useState(false);
-    
+    const [products, setProducts] = useState([]);
+    const product = products.find((product) => product.id === product.id); 
+
+    useEffect(() => {
+        fetchProducts().then((data) => {
+            setProducts(data);
+        });
+    }, []);
 
     function openModal() {
         setIsOpen(true);
@@ -12,19 +21,61 @@ export default function ProductModal() {
         setIsOpen(false);
     }
 
+
     return (
         <div>
-            <button onClick={openModal}>Product Modal</button>
+            <div className="REA"> </div>
+            <div className="navbar"> </div>
+            <div className="products-categories"> </div>
+            <div className="product-container">
+                {products.filter((product) => product.id === 1)
+                    .map((product) => (
+                        <div key={product.id} className="product-card">
+                        <img src={product.images} alt={product.name} />  
+                        <p className="product-brand">Brand: {product.brand}</p>    
+                        <h3 className="product-name">{product.name}</h3>
+                        <p className="product-description">{product.description}</p>
+                        <p className="product-price">Price: ${product.price}</p>
+                        <p className="product-delivery">Delivery: {product.shippingInformation}</p>
+                        <p className="product-discount">Discount: ${product.discount}</p>
+                        <p className="product-stock" hidden>Stock: {product.stock}</p> 
+                        <p className="product-availability-status" >Availability: {product.availabilityStatus}</p>
+                        <p className="product-rating">Rating: {product.rating}</p>
+                        <p className="product-return-policy">Return Policy: {product.returnPolicy}</p>
+                            
+                    </div>
+                ))}
+            </div>
+            <button>Add to cart</button>
+            <button onClick={openModal}>Buy</button>
             {isOpen && (
-                <div className="modal">
-                    <div className="modal-product">
+                <div className="cart-container">
+                    <div className="cart">
                         <span className="close" onClick={closeModal}>
                             &times;
                         </span>
-                        <p>Modal Content</p>
                     </div>
                 </div>
             )}
+        
+            <button>Favorites</button> 
+            <div className="bye-together">
+                <h4>Products related to this item</h4>
+                {products.filter((product) => product.category === "beauty")
+                    .slice(0, 4) // Limit to 4 products
+                    .map((product) => (
+                        <div key={product.id} className="product-card">
+                        <img src={product.images} alt={product.name} />  
+                        <h3 className="product-name">{product.name}</h3>
+                        <p className="product-price">Price: ${product.price}</p>
+                        <p className="product-delivery">Delivery: {product.shippingInformation}</p>
+                        <p className="product-rating">Rating: {product.rating}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="footer"> </div>
         </div>
     );
 }
+
+
