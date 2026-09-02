a69da688a2d44556fd94543ef5fa271a9aa5ab6a
Viktor Gustafsson <viktor.gustafsson1@hotmail.com>
2026-09-02 12:33:08 +0200

    feat: add mainCategories array with categories and subcategories

---
+++ src/utils/mainCategories.js
@@ -0,0 +1,28 @@
+const mainCategories = [
+  { name: "Clothes", subCategories: ["mens-shirts", "tops", "womens-dresses"] },
+  { name: "Shoes", subCategories: ["mens-shoes", "womens-shoes"] },
+  {
+    name: "Accessories",
+    subCategories: [
+      "sunglasses",
+      "womens-bags",
+      "womens-jewellery",
+      "mens-watches",
+      "womens-watches",
+    ],
+  },
+  { name: "Beauty", subCategories: ["beauty", "fragrances", "skin-care"] },
+  {
+    name: "Home",
+    subCategories: ["furniture", "home-decoration", "kitchen-accessories"],
+  },
+  { name: "Groceries", subCategories: ["groceries"] },
+  {
+    name: "Electronics",
+    subCategories: ["laptops", "smartphones", "tablets", "mobile-accessories"],
+  },
+  { name: "Vehicles", subCategories: ["motorcycle", "vehicle"] },
+  { name: "Sports", subCategories: ["sports-accessories"] },
+];
+
+export default mainCategories;
