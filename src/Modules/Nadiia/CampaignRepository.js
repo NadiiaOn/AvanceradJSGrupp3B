b852f0516d6b00c634018bc6a4cb2ab3d519872d
NadiiaOn <nadiia.onopriienko@gmail.com>
2026-09-09 13:01:26 +0200

    implement fetchPercentageCampaigns

--- src/Modules/Nadiia/CampaignRepository.js
+++ src/Modules/Nadiia/CampaignRepository.js
@@ -1,3 +1,29 @@
-// This class is responsible for handling all data from the database related to campigns and discounts.  
+// This class is responsible for handling all data from the database related to campigns and discounts.
 
-//It will take db throw the API. 
+//It will take db throw the API.
+
+const fetchPercentageCampaigns = async () => {
+  const response = await fetch("/api/percentageCampaigns");
+  console.log(
+    "URL:",
+    response.url,
+    "| status:",
+    response.status,
+    "| ok:",
+    response.ok,
+  );
+
+  if (!response.ok) {
+    throw new Error("Error: Kunde inte hämta kampanjerna från servern!");
+  }
+
+  const result = await response.json();
+
+  if (!Array.isArray(result) || result.length === 0) {
+    throw new Error("Inga kampanjen hittades i databasen.");
+  }
+
+  return result[0];
+};
+
+export default fetchPercentageCampaigns;
