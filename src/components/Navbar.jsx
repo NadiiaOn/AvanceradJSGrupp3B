a372c0d53f17c456727410830e2785a4745fcf6f
Viktor Gustafsson <viktor.gustafsson1@hotmail.com>
2026-09-11 23:02:27 +0200

    feat: add currency selector button to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,20 @@ import {
   ListIcon,
   UserIcon,
   MagnifyingGlassIcon,
+  MoneyIcon,
 } from "@phosphor-icons/react";
 import NavMenu from "./NavMenu";
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { useCart } from "../context/CartContext";
+import CurrencySelector from "./CurrencySelector";
+import { useCurrency } from "../context/CurrencyContext";
 
 export default function Navbar({ products, errorMessage }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isCurrencyOpen, setIsCurrencyOpen] = useState(false);
   const { toggleCart, totalItems } = useCart();
+  const { currency, setCurrency } = useCurrency();
   const navigate = useNavigate();
 
   return (
@@ -53,8 +58,24 @@ export default function Navbar({ products, errorMessage }) {
               <p className="hidden sm:block text-text font-body">Search</p>
             </button>
           </div>
-          {/* Kundvagn */}
-          <div className="flex gap-2 sm:order-3 sm:justify-self-end">
+          {/* Kundvagn och valuta*/}
+          <div className="flex items-center gap-2 sm:order-3 sm:justify-self-end">
+            <div className="relative">
+              <button
+                onClick={() => setIsCurrencyOpen((prev) => !prev)}
+                className="cursor-pointer xl:flex xl:items-center xl:gap-1 py-2 px-2 rounded-full hover:bg-card"
+              >
+                <img
+                  src={`${currency === "SEK" ? "/iconsek.png" : currency === "EUR" ? "/iconeur.png" : "/iconusd.png"}`}
+                  alt=""
+                  className="w-6.5 h-6.5 xl:w-8 xl:h-8"
+                />
+              </button>
+              {isCurrencyOpen && (
+                <CurrencySelector onClose={() => setIsCurrencyOpen(false)} />
+              )}
+            </div>
+
             <button className="hidden cursor-pointer xl:flex xl:items-center xl:gap-1 py-2 px-2 rounded-full hover:bg-card">
               <UserIcon weight="thin" className="w-6.5 h-6.5 xl:w-8 xl:h-8" />
               <p className="text-text font-body">Log In</p>
