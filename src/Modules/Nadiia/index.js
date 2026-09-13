3153f4e7795731968f027590e11d17d3520414a1
NadiiaOn <nadiia.onopriienko@gmail.com>
2026-09-13 21:04:12 +0200

    improve the structure of the project

--- src/Modules/Nadiia/index.js
+++ src/Modules/Nadiia/index.js
@@ -1,34 +1,6 @@
 // The main file for the Nadiia module, which exports all the campaign functions.
-import { campaignFactory } from "./DiscountCampaigns.js";
 
-const CART_KEY = "cart";
-
-export function getCart() {
-  let cartItems = [];
-  try {
-    cartItems = JSON.parse(localStorage.getItem(CART_KEY)) ?? [];
-  } catch {
-    cartItems = [];
-  }
-
-  const totalPrice = cartItems.reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0,
-  );
-
-  return { cartItems, totalPrice };
-}
-
-export async function getDiscountedTotal() {
-  const res = await fetch("/api/products");
-  const { cartItems, totalPrice } = getCart();
-  if (!res.ok) return totalPrice;
-
-  const campaign = CampaignIerarchy(await res.json());
-
-  return campaign.calculateDiscountedPriceForCart(cartItems, totalPrice);
-}
-
+// implement db in input in it I will use it
 export default class DiscountCampaignsModule {
   
   static descriptor = {
