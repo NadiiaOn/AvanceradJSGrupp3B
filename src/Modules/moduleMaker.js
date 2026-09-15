edce35a7365a793b075b3e20fa4b9b0ad1cc3cea
JoelSRJE <JoelSRJE@hotmail.com>
2026-09-15 14:40:32 +0200

    Updated db, vscode flipped out but it works

--- src/Modules/moduleMaker.js
+++ src/Modules/moduleMaker.js
@@ -1,7 +1,9 @@
 import ShippingQuoteModule from "./shippingCalculator/index.js";
+import InventoryModule from "./Inventory/InventoryModule.js";
 
 export default {
   ShippingQuote: new ShippingQuoteModule(),
   ShippingQuoteDescriptor: ShippingQuoteModule.descriptor,
-
+  InventoryModule: new InventoryModule(),
+  InventoryModuleDescriptor: InventoryModule.descriptor,
 };
